756035c925a29b1eeff80e3a66cc2ae5bb8e785d
Aakash467 <140136468+Aakash467@users.noreply.github.com>
2025-05-23 21:37:45 +0530

    Update api.js

--- client/utils/api.js
+++ client/utils/api.js
@@ -1,6 +1,6 @@
 // import axios from "axios";
 
-const API_URL = "http://localhost:5001/api";
+const API_URL = "https://coding-tracker.onrender.com";
 
 // export const fetchUserStats = async (userId) => {
 //   try {
